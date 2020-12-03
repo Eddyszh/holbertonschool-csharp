4f3ce4b4dd3aee0b71c4c0db537817075828eca4
eddyszh <eddyszh@gmail.com>
2020-12-02 20:20:30 -0500

    Add advanced task

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row;
+        int col = 3;
+        int[][] jagged = new int[][]
+        {
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+        for (int i = 0; i < col; i++)
+        {
+            row = jagged[i].Length;
+            for (int j = 0; j < row; j++)
+                Console.Write((j != row - 1) ? $"{j} " : $"{j}\n");
+        }
+    }
+}
