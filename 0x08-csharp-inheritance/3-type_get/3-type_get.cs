80a34629437cf2cd020664ab92902fc2985fbe0c
eddyszh <eddyszh@gmail.com>
2020-12-21 19:19:45 -0500

    Add c# inheritance project

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type info = myObj.GetType();
+        Console.WriteLine("{0} Properties: ", info.Name);
+        foreach (PropertyInfo item in info.GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+        Console.WriteLine("{0} Methods: ", info.Name);
+        foreach (MethodInfo item in info.GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
